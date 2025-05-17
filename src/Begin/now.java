c89bb94c721069081b1600239bbe21994d44a2ea
Vaishnav135 <khandekarvaishnav9@gmail.com>
2025-05-17 12:38:52 +0530

    Initial stage

---
+++ src/Begin/now.java
@@ -0,0 +1,31 @@
+package Begin;
+
+public class now {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			int[] a={1,3,45,6,8,9};
+			
+			int max=a[0], min=a[0];
+			
+			for(int num:a)
+			{
+				if(num>max)
+					max=num;
+				
+				if(num<min)
+					max=num;
+				
+				
+				
+			}
+		
+			
+			System.out.println("Max number is " + max + "Min number is  "+ min);
+			
+			
+		}
+	}
+
+}
